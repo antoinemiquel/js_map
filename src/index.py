b9cc4243c45447b1bf71acf319d8953dbee6c123
antoine <antoine.miquel@neuf.fr>
2016-06-12 14:48:27 +0200

    correct details

--- src/index.py
+++ src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, jsonify
+from flask import Flask, render_template, Response
 from pymongo import MongoClient
 from bson.json_util import dumps
 import json
